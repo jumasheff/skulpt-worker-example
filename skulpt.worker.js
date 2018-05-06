0b327fc2b2f91d4186abd5bbd69da5e715cd6542
jumasheff <jumasheff@gmail.com>
2018-05-06 15:16:23 +0600

    Initial commit

---
+++ skulpt.worker.js
@@ -0,0 +1,40 @@
+if ('function' === typeof importScripts) {
+
+    self.importScripts('skulpt.min.js')
+    self.importScripts('skulpt-stdlib.js')
+
+    let readModule = module => (
+        self.Sk.builtinFiles["files"][module]
+    );
+
+    let sendMessage = (name, message) => {
+        self.postMessage(JSON.stringify({
+            type: name,
+            data: message
+        }));
+    }
+
+    let run = source => {
+        Sk.configure({
+            read: readModule,
+            output: function (output) {
+                sendMessage('stdout', output);
+            }
+        });
+
+        try {
+            Sk.importMainWithBody("<stdin>", false, source);
+        } catch (e) {
+            sendMessage('stderr', {
+                exception: e.tp$name,
+                description: String(e)
+            });
+        }
+
+        sendMessage('exit');
+    }
+
+    self.addEventListener('message', (e) => {
+        run(e.data)
+    });
+}
---
+++ worker-runner.js
@@ -0,0 +1,28 @@
+let stdOutEl = document.getElementById('output');
+let stdErrEl = document.getElementById('error');
+let codeInput = document.getElementById('code_editor');
+
+function runWorker() {
+    let code = codeInput !== null ? codeInput.value : '';
+
+    if (typeof(w) == 'undefined') {
+        var w = new Worker('skulpt.worker.js')
+    }
+
+    w.onmessage = function(event) {
+        let data = JSON.parse(event.data);
+        if (data.type === 'exit') {
+            w.terminate();
+            w = undefined;
+        }
+        if (data.type === 'stdout') {
+            console.log(data.data);        
+            stdOutEl.innerText += data.data;
+        }
+        if (data.type === 'stderr') {
+            console.log('Error:', data.data);
+            stdErrEl.innerText += data.data;
+        }
+    }
+    w.postMessage(code)
+}
